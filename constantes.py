11bf4c33e05de8fc578c1dc60d3adc5b4fbca5b1
Pablo-Ali <pablosharifali@gmail.com>
2024-11-21 16:44:28 -0300

    feat: agregadas constantes, agregados comodines en el jugador, llamado a la pantalla menú en el main, desarrollada la lógica de la pantalla menú

--- constantes.py
+++ constantes.py
@@ -8,18 +8,41 @@ ALTO = 600
 VENTANA = (ANCHO,ALTO)
 FPS = 60
 
-# Dificultades
-# Normal
-CANTIDAD_VIDAS_NORMAL = 3
-PUNTUACION_ACIERTO_NORMAL = 100
-PUNTUACION_ERROR_NORMAL = 50
+# Jugador
+CANTIDAD_VIDAS = 3
+PUNTUACION_ACIERTO = 100
+PUNTUACION_ERROR = 50
 
-# Fácil
-CANTIDAD_VIDAS_FACIL = 5
-PUNTUACION_ACIERTO_FACIL = 100
-PUNTUACION_ERROR_FACIL = 25
+# Audio
+MUSICA_MENU = pygame.mixer.Sound("recursos/audio/Pokemon_Route_1.mp3")
+MUSICA_JUEGO = pygame.mixer.Sound("recursos/audio/Pokemon_Opening.mp3")
+MUSICA_RANKINGS = pygame.mixer.Sound("recursos/audio/Pokemon_Pallet_Town.mp3")
+MUSICA_CONFIGURACION = pygame.mixer.Sound("recursos/audio/Pokemon_Trainer_Battle.mp3")
+MUSICA_PARTIDA_FINALIZADA = pygame.mixer.Sound("recursos/audio/Pokemon_Ending.mp3")
 
-# Difícil
-CANTIDAD_VIDAS_DIFICIL = 1
-PUNTUACION_ACIERTO_DIFICIL = 100
-PUNTUACION_ERROR_DIFICIL = 100
+# Fondos
+FONDO_MENU = pygame.transform.scale(pygame.image.load("recursos/imagenes/fondo_9.jpg"), VENTANA)
+
+
+# Botones del menú
+BOTON_JUGAR = 0
+BOTON_CONFIG = 1
+BOTON_RANKINGS = 2
+BOTON_SALIR = 3
+
+# Rutas opciones
+RUTA_IMAGENES_BOTONES = [
+    "recursos/imagenes/carta_fuego.png",
+    "recursos/imagenes/carta_agua.png",
+    "recursos/imagenes/carta_electrico.png",
+    "recursos/imagenes/carta_psiquico.png"
+]
+RUTA_IMAGENES_RESPUESTAS = [
+    "recursos/imagenes/" #COMPLETAR
+    "recursos/imagenes/"
+    "recursos/imagenes/"
+    "recursos/imagenes/"
+]
+
+COLOR_NEGRO = (0,0,0)
+FUENTE_25 = pygame.font.SysFont("Arial Narrow",25)
--- pantalla_menu.py
+++ pantalla_menu.py
@@ -3,3 +3,51 @@ import constantes
 import funciones_generales
 
 pygame.init()
+
+lista_botones = []
+
+for i in range(4):
+    boton = {}
+    imagen_original = boton["superficie"] = pygame.image.load(constantes.RUTA_IMAGENES_BOTONES[i])
+    boton["superficie"] = pygame.transform.scale(imagen_original, (126, 177)) # 30% de la imagen original
+    boton["rectangulo"] = boton["superficie"].get_rect()
+    boton["rectangulo"].topleft = (35 + i * 200, 50 )
+    lista_botones.append(boton)
+
+def mostrar_menu(pantalla:pygame.Surface,cola_eventos:list[pygame.event.Event]) -> str:
+    # Nombre de la ventana
+    pygame.display.set_caption("MENU")
+
+    retorno = "menu"
+    
+    # Gestion Eventos
+    for evento in cola_eventos:
+        if evento.type == pygame.QUIT:
+            retorno = "salir"
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            for i in range(len(lista_botones)):
+                if lista_botones[i]["rectangulo"].collidepoint(evento.pos):
+                    if i == constantes.BOTON_JUGAR:
+                        retorno = "juego"
+                    elif i == constantes.BOTON_CONFIG:
+                        retorno = "configuraciones"
+                    elif i == constantes.BOTON_RANKINGS:
+                        retorno = "rankings"
+                    elif i == constantes.BOTON_SALIR:
+                        retorno = "salir"
+
+    # Dibujar fondo de pantalla
+    pantalla.blit(constantes.FONDO_MENU, (0, 0))
+
+    # Dibujar los botones
+    for boton in lista_botones:
+        pantalla.blit(boton["superficie"], boton["rectangulo"])
+
+    funciones_generales.mostrar_texto(lista_botones[constantes.BOTON_JUGAR]["superficie"],"JUGAR",(30,120),constantes.FUENTE_25,constantes.COLOR_NEGRO)
+    funciones_generales.mostrar_texto(lista_botones[constantes.BOTON_CONFIG]["superficie"],"CONFIGURAR",(5,120),constantes.FUENTE_25,constantes.COLOR_NEGRO)
+    funciones_generales.mostrar_texto(lista_botones[constantes.BOTON_RANKINGS]["superficie"],"RANKINGS",(15,120),constantes.FUENTE_25,constantes.COLOR_NEGRO)
+    funciones_generales.mostrar_texto(lista_botones[constantes.BOTON_SALIR]["superficie"],"SALIR",(35,120),constantes.FUENTE_25,constantes.COLOR_NEGRO)
+
+    #pygame.display.flip()
+
+    return retorno
