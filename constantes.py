ea2f582f68777bf13848bccf137a4a2ad5a98b79
Pablo-Ali <pablosharifali@gmail.com>
2024-11-22 22:26:45 -0300

    update: agregadas constantes de rutas

--- constantes.py
+++ constantes.py
@@ -15,6 +15,10 @@ PUNTUACION_ERROR = 50
 
 # Fondos
 FONDO_MENU = pygame.transform.scale(pygame.image.load("recursos/imagenes/fondo_9.jpg"), VENTANA)
+FONDO_CONFIGURACIONES = pygame.transform.scale(pygame.image.load("recursos/imagenes/fondo_3.jpg"), VENTANA)
+FONDO_JUEGO = pygame.transform.scale(pygame.image.load("recursos/imagenes/estadio.jpg"), VENTANA)
+FONDO_RANKINGS = pygame.transform.scale(pygame.image.load("recursos/imagenes/fondo_7.jpg"), VENTANA)
+
 
 # Botones del menú
 BOTON_JUGAR = 0
@@ -25,12 +29,14 @@ BOTON_SALIR = 3
 # Rutas
 
 # Imagenes
-RUTA_IMAGENES_BOTONES = [
+RUTA_IMAGENES_BOTONES_MENU = [
     "recursos/imagenes/carta_fuego.png",
     "recursos/imagenes/carta_agua.png",
     "recursos/imagenes/carta_electrico.png",
     "recursos/imagenes/carta_psiquico.png"
 ]
+
+RUTA_IMAGEN_BOTON_CONFIG = "recursos/imagenes/fondo_10.jpg"
 
 RUTA_IMAGENES_RESPUESTAS = [
     "recursos/imagenes/" #COMPLETAR
@@ -39,13 +45,19 @@ RUTA_IMAGENES_RESPUESTAS = [
     "recursos/imagenes/"
 ]
 
+
 # Audio
 MUSICA_MENU = "recursos/audio/Pokemon_Route_1.mp3"
 MUSICA_JUEGO = "recursos/audio/Pokemon_Opening.mp3"
-MUSICA_RANKINGS = "recursos/audio/Pokemon_Pallet_Town.mp3)"
+MUSICA_RANKINGS = "recursos/audio/Pokemon_Pallet_Town.mp3"
 MUSICA_CONFIGURACION = "recursos/audio/Pokemon_Trainer_Battle.mp3"
 MUSICA_PARTIDA_FINALIZADA = "recursos/audio/Pokemon_Ending.mp3"
 
 # Fuentes
-COLOR_NEGRO = (0,0,0)
 FUENTE_25 = pygame.font.SysFont("Arial Narrow",25)
+FUENTE_30 = pygame.font.SysFont("Arial Narrow",30)
+
+# Colores
+COLOR_NEGRO = (0,0,0)
+COLOR_BLANCO = (255,255,255)
+COLOR_AMARILLO = (255, 255, 0)
