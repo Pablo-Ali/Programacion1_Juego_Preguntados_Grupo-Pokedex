0072674351fd9f5671be762d0ab8f7538ce2af60
Pablo-Ali <pablosharifali@gmail.com>
2024-11-22 22:27:11 -0300

    update: agregada documentación

--- funciones_generales.py
+++ funciones_generales.py
@@ -3,9 +3,6 @@ import random
 
 
 def mostrar_texto(surface, text, pos, font, color=pygame.Color('black')):
-    '''
-    
-    '''
     words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
     space = font.size(' ')[0]  # The width of a space.
     max_width, max_height = surface.get_size()
@@ -24,13 +21,14 @@ def mostrar_texto(surface, text, pos, font, color=pygame.Color('black')):
     
 def mezclar_lista(lista:list) -> None:
     '''
-
+    Función que recibe una lista y mezcla sus elementos.
     '''
     random.shuffle(lista)
 
 def generar_flotante_musica(volumen : int) -> float:
     '''
-    
+    Función que recibe un entero y lo divide por mil
+    para generar un flotante y lo retorna.
     '''
     # Generamos un flotante a partir del entero cargado en el jugador
     volumen_float = volumen / 1000 # dividimos por 1000 ya que el volumen es demasiado alto
@@ -39,7 +37,9 @@ def generar_flotante_musica(volumen : int) -> float:
 
 def iniciar_musica(ruta : str, volumen : int) -> None:
     '''
-    
+    Función que recibe la ruta de una pista musical y un entero.
+    Carga la musica en el mixer de pygame, establece su volumen
+    y la reproduce.
     '''
     # Generamos el float del volumen
     volumen_float = generar_flotante_musica(volumen)
@@ -52,6 +52,8 @@ def iniciar_musica(ruta : str, volumen : int) -> None:
 
 def cambiar_volumen_musica(volumen : int) -> None:
     '''
+    Función que permite modificar el volumen de la música
+    a partir de un entero pasado por parámetro.
     '''
     volumen_float = generar_flotante_musica(volumen)
     pygame.mixer.music.set_volume(volumen_float)
