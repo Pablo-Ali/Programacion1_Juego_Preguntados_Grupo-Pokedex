a1dc936225820278a0e2e14ad2bf70c58800b955
Pablo-Ali <pablosharifali@gmail.com>
2024-11-22 22:28:40 -0300

    ya no recuerdo qué hice

--- pantalla_menu.py
+++ pantalla_menu.py
@@ -8,13 +8,13 @@ lista_botones = []
 
 for i in range(4):
     boton = {}
-    imagen_original = boton["superficie"] = pygame.image.load(constantes.RUTA_IMAGENES_BOTONES[i])
+    imagen_original = boton["superficie"] = pygame.image.load(constantes.RUTA_IMAGENES_BOTONES_MENU[i])
     boton["superficie"] = pygame.transform.scale(imagen_original, (126, 177)) # 30% de la imagen original
     boton["rectangulo"] = boton["superficie"].get_rect()
     boton["rectangulo"].topleft = (35 + i * 200, 50 )
     lista_botones.append(boton)
 
-def mostrar_menu(pantalla:pygame.Surface,cola_eventos:list[pygame.event.Event]) -> str:
+def mostrar_menu(pantalla:pygame.Surface, cola_eventos:list[pygame.event.Event]) -> str:
     # Nombre de la ventana
     pygame.display.set_caption("MENU")
 
@@ -43,11 +43,10 @@ def mostrar_menu(pantalla:pygame.Surface,cola_eventos:list[pygame.event.Event]) 
     for boton in lista_botones:
         pantalla.blit(boton["superficie"], boton["rectangulo"])
 
+    # Mostrar texto en los botones
     funciones_generales.mostrar_texto(lista_botones[constantes.BOTON_JUGAR]["superficie"],"JUGAR",(30,120),constantes.FUENTE_25,constantes.COLOR_NEGRO)
     funciones_generales.mostrar_texto(lista_botones[constantes.BOTON_CONFIG]["superficie"],"CONFIGURAR",(5,120),constantes.FUENTE_25,constantes.COLOR_NEGRO)
     funciones_generales.mostrar_texto(lista_botones[constantes.BOTON_RANKINGS]["superficie"],"RANKINGS",(15,120),constantes.FUENTE_25,constantes.COLOR_NEGRO)
     funciones_generales.mostrar_texto(lista_botones[constantes.BOTON_SALIR]["superficie"],"SALIR",(35,120),constantes.FUENTE_25,constantes.COLOR_NEGRO)
-
-    #pygame.display.flip()
 
     return retorno
